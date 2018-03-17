af224387496195a9686f00de9d30f81e6cdbe8e6
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-03-17 23:25:52 +1100

    Initial commit

---
+++ heap_sort/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * C# Program to Demonstrate Heap Sort
+ */
+
+using System;
+
+namespace heap_sort
+{
+    class Heap
+    {
+        int[] r = { 2, 5, 1, 10, 6, 9, 3, 7, 4, 8 };
+        public void hsort()
+        {
+            int i, t;
+            for (i = 5; i >= 0; i--)
+            {
+                adjust(i, 9);
+            }
+            for (i = 8; i >= 0; i--)
+            {
+                t = r[i + 1];
+                r[i + 1] = r[0];
+                r[0] = t;
+                adjust(0, i);
+            }
+        }
+    }
+}
